81b818b2b9923c0dc72380351a439953e6847458
Daniel Heringer <danielheringers@gmail.com>
2024-08-20 11:23:31 -0300

    headers

--- app/Routes/pdf_generator.py
+++ app/Routes/pdf_generator.py
@@ -1,16 +1,52 @@
-from fastapi import APIRouter, HTTPException, Body, Header, Depends
+from fastapi import APIRouter, HTTPException, Body, Header, Depends, Request
 from fastapi.responses import StreamingResponse, JSONResponse
 from app.Utils.Danfe.danfe_utils import create_pdf
 from app.Models.Danfe.models import Danfe
 from app.Models.Errors.errors import custom_error_response
+import uuid
+from datetime import datetime
 
 router = APIRouter()
 
 def verify_headers(
-    tenantid: str = Header(...),
-    username: str = Header(...),
-    useremail: str = Header(...)
+    tenantid: str = Header(None),
+    username: str = Header(None),
+    useremail: str = Header(None)
 ):
+    if tenantid is None:
+        error_response = custom_error_response(
+            code=400,
+            message="Bad Request",
+            code_error="ORBIT_10001",
+            msg="tenantid is required",
+            location="header",
+            property_name="tenantid",
+            value=None
+        )
+        raise HTTPException(status_code=400, detail=error_response["message"], headers=error_response)
+    if username is None:
+        error_response = custom_error_response(
+            code=400,
+            message="Bad Request",
+            code_error="ORBIT_10002",
+            msg="username is required",
+            location="header",
+            property_name="username",
+            value=None
+        )
+        raise HTTPException(status_code=400, detail=error_response["message"], headers=error_response)
+    if useremail is None:
+        error_response = custom_error_response(
+            code=400,
+            message="Bad Request",
+            code_error="ORBIT_10003",
+            msg="useremail is required",
+            location="header",
+            property_name="useremail",
+            value=None
+        )
+        raise HTTPException(status_code=400, detail=error_response["message"], headers=error_response)
+
     return {"tenantid": tenantid, "username": username, "useremail": useremail}
 
 @router.post("/generate-danfe-pdf")
@@ -27,14 +63,7 @@ def create_danfe_pdf_endpoint(
             headers={"Content-Disposition": "inline; filename=document.pdf"}
         )
     except HTTPException as e:
-        error_response = custom_error_response(
-            code=e.status_code,
-            message="HTTP Error",
-            code_error="ORBIT_10001",
-            msg=e.detail,
-            location="body"
-        )
-        return JSONResponse(status_code=e.status_code, content=error_response)
+        return JSONResponse(status_code=e.status_code, content=e.headers)
     except Exception as e:
         error_response = custom_error_response(
             code=500,
