19ed4e8defc48195e4184e4413048d959de088b0
Daniel Heringer <danielheringers@gmail.com>
2024-09-25 17:53:07 -0300

    Versão_1

--- app/api/router/routes.py
+++ app/api/router/routes.py
@@ -1,40 +1,12 @@
 from typing import List
-from fastapi import APIRouter, Body, HTTPException, Header, Depends
-from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi import APIRouter, Body, Depends
+from fastapi.responses import StreamingResponse
 from app.schemas.boleto.models import Boleto
 from app.services.boletos.boleto_render import create_pdf_boleto
-# from app.services.danfe.danfe_utils import create_pdf_danfe
-# from app.schemas.danfe.models import Danfe
-from app.schemas.errors.custom_exception import HeaderMissingException
+from app.core.security import verify_headers
 
 router = APIRouter()
 
-def verify_headers(
-    tenantid: str = Header(None),
-):
-    missing_headers = []
-    if tenantid is None:
-        missing_headers.append("tenantid")
-        
-    if missing_headers:
-        raise HeaderMissingException(missing_headers)
-    
-    return {"tenantid": tenantid}
-
-# @router.post("/gerar/pdf/danfe")
-# async def create_danfe_pdf_endpoint(
-#     data: Danfe = Body(...),
-#     headers: dict = Depends(verify_headers)
-# ):
-#     pdf_buffer = create_pdf_danfe(data)
-#     pdf_buffer.seek(0)
-#     return StreamingResponse(
-#         pdf_buffer, 
-#         media_type='application/pdf', 
-#         headers={"Content-Disposition": f'inline; filename={data.identificacao.codigoNf}.pdf'}
-#     )
-    
-    
 @router.post("/gerar/pdf/boleto")
 async def create_bankslip_pdf_endpoint(
     data: List[Boleto] = Body(...), 
